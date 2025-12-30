63b6ead5c35537c05b135bbece8dc18bcdea9234
Philipe Félix <philipe.foliveira@gmail.com>
2025-12-30 18:51:32 -0300

    Exercício de média final do aluno.

---
+++ 01_fundamentos/03_media_final _do _aluno.py
@@ -0,0 +1,9 @@
+# Média final do aluno
+
+nota_1 = float(input("Digite a sua primeira nota: "))
+nota_2 = float(input("Digite a sua segunda nota: "))
+nota_3 = float(input("Digite a sua terceira nota: "))
+nota_4 = float(input("Digite a sua quarta nota: "))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+print(f"{media:.2f}")
