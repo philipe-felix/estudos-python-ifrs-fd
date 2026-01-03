6f8bdc0bd86517c28a3102f5c7fe63e0c2d3e6a9
Philipe Félix <philipe.foliveira@gmail.com>
2026-01-03 18:50:15 -0300

    Exercício de saldo da carteira

---
+++ 01_fundamentos/06_saldo_na_carteira.py
@@ -0,0 +1,6 @@
+# Saldo na Carteira
+
+saldo_da_carteira = int(input("Informe o saldo da carteira: "))
+valor_do_servico = int(input("Informe o valor do serviço prestado: "))
+total = saldo_da_carteira + valor_do_servico
+print(total)
