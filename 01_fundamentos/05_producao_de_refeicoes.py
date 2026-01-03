9d1677f29a99ca53bd44e92970f721aa485fe1c2
Philipe Félix <philipe.foliveira@gmail.com>
2026-01-03 18:40:27 -0300

    Exercício de Produção de refeições

---
+++ 01_fundamentos/05_producao_de_refeicoes.py
@@ -0,0 +1,6 @@
+# Produção de refeições
+
+quantidade_viajantes = int(input("Informe a quantidade de viajantes: "))
+quantidade_dias = int(input("Informe a quantidade de dias da viagem: "))
+total_de_refeicoes = quantidade_viajantes * 3
+print(total_de_refeicoes * quantidade_dias)
